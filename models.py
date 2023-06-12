54fdaa62b8f06832d3c19727366f839fa48f2827
name <ksyusha0kubay@gmail.com>
2023-06-12 18:43:24 +0300

    Add requirements.txt

--- models.py
+++ models.py
@@ -1,13 +1,17 @@
-from datetime import datetime
-from sqlalchemy import Column, Integer, Float, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from config import db
 
 
-class MarketData(Base):
-    __tablename__ = 'market_data'
-    id = Column(Integer, primary_key=True)
-    date = Column(DateTime, default=datetime.now)
-    price = Column(Float)
-    volume = Column(Float)
+class MarketData(db.Model):
+    __tablename__ = "market_data"
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    hour = db.Column(db.String)
+    price = db.Column(db.Float)
+    volume = db.Column(db.Float)
+
+    def __init__(self, date, hour, price, volume):
+        self.date = date
+        self.hour = hour
+        self.price = price
+        self.volume = volume
